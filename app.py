326e9c5dc06b200fafb7c8e4b860ff8d760505a1
Erob494 <123705191+Erob494@users.noreply.github.com>
2023-03-23 00:07:33 -0400

    Update app.py
    done
    
    Signed-off-by: Erob494 <123705191+Erob494@users.noreply.github.com>

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    movies = movie_repository
+    return render_template('list_all_movies.html', movie = movies)
 
 
 @app.get('/movies/new')
